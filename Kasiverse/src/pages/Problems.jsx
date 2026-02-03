e97af299bab249289480ec188036d0cc2c73d6a2
mellow874 <felicitymncube49@gmail.com>
2026-02-03 20:55:55 +0200

    added more objects of array

--- Kasiverse/src/pages/Problems.jsx
+++ Kasiverse/src/pages/Problems.jsx
@@ -26,6 +26,28 @@ function Problems() {
         "Many parents work late and need affordable, safe after-school tutoring for their children in primary school.",
       location: "Alexandra",
     },
+    {
+      id: 3,
+      title: "Crime and Safety",
+      description:
+        "High crime rates make it unsafe to walk around at night, with incidents of theft, burglary, and gang violence being common.",
+      location: "Vaal",
+    },
+    {
+  id: 4,
+  title: "Electricity Load Shedding",
+  description:
+    "Communities experience frequent power outages, which disrupts small businesses, studying, and household routines.",
+  location: "Townships in Gauteng",
+},
+    {
+  id: 5,
+  title: "Fitness & Recreation",
+  description:
+    "People lack safe areas to exercise or play sports. Starting community gyms, classes, or sports programs could help.",
+  location: "Brakpan",
+},
+
   ]);
 
   // State for search input value
@@ -61,20 +83,22 @@ function Problems() {
     // Page container
     <div>
       <Navbar/>
-      <div className="min-h-screen bg-linear-to-l from-gray-800 via-blue-700 to-gray-900 px-8 pt-24 pb-8">
+      <div className="min-h-screen bg-linear-to-l from-purple-800 via-black to-purple-900 px-8 pt-24 pb-8">
       {/* Page title */}
-      <h1 className="text-3xl font-bold text-white mb-1">
+      
+      <h1 className=" text-3xl font-bold text-white mb-1">
         Community Problem Board
       </h1>
       {/* Subtitle */}
       <p className="text-gray-300 mb-6">
         Share problems affecting your community.
       </p>
+      
 
       {/* Top bar */}
-      <div className="flex items-center justify-between gap-4 backdrop-blur-md">
+      <div className="flex items-center justify-between gap-4 ">
         {/* Search input */}
-        <div className="flex items-center bg-white/80 gap-2 w-full px-12 py-3 rounded-md max-w-md">
+        <div className="flex items-center bg-white/80 gap-2 w-full max-w-auto px-12 py-3 rounded-md">
           <Search size={18} />
           <input
             type="text"
@@ -88,15 +112,16 @@ function Problems() {
         {/* Add problem button */}
         <button
           onClick={() => setIsModalOpen(true)}
-          className="bg-black text-white px-4 py-2 rounded-md transition duration-300 ease-out hover:bg-linear-to-l from-gray-800 via-blue-700 to-gray-900 p-8"
+          className="bg-linear-to-l from-purple-800 via-black to-purple-900 text-white w-40 px-4 py-3  rounded-md transition duration-300 ease-out"
         >
           + Add Problem
         </button>
 
       </div>
+    
 
       {/* Problems grid */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 p-3 mt-8 max-w-8xl mx-auto">
         {filteredProblems.map((problem) => (
           <ProblemPanel
             key={problem.id}
@@ -113,6 +138,7 @@ function Problems() {
         />
       )}
     </div>
+ 
     </div>
   );
 }
